85d37a1ed9fffcd0505c86cb4c2d5de2b5c38be5
Grant Mathews <grant@causes.com>
2011-12-16 12:50:10 -0800

    Initial commit to ld22.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, pygame
+import sys, pygame, spritesheet, wordwrap
+
 pygame.init()
 
 WIDTH = HEIGHT = 500
@@ -10,6 +11,41 @@ def get_uid():
   get_uid.uid += 1
   return uid
 get_uid.uid = 0
+
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __cmp__(self, other):
+    return 0 if self.x == other.x and self.y == other.y else 1
+
+  def __str__(self):
+    return "<Point x : %f y : %f>" % (self.x, self.y)
+
+class TileSheet:
+  """ Memoize all the sheets so we don't load in 1 sheet like 50 times and 
+  squander resources. This is a singleton, which is generally frowned upon, 
+  but I think it's okay here."""
+  sheets = {}
+
+  @staticmethod
+  def add(file_name):
+    if file_name in TileSheet.sheets:
+      return
+
+    new_sheet = spritesheet.spritesheet(file_name)
+    width, height = dimensions = new_sheet.sheet.get_size()
+    TileSheet.sheets[file_name] =\
+     [[new_sheet.image_at((x, y, TILE_SIZE, TILE_SIZE), colorkey=(255,255,255))\
+       for y in range(0, height, TILE_SIZE)] for x in range(0, width, TILE_SIZE)]
+
+  @staticmethod
+  def get(sheet, x, y):
+    if sheet not in TileSheet.sheets:
+      raise
+    return TileSheet.sheets[sheet][x][y]
 
 class Entity(object):
   def __init__(self, x, y, size):
@@ -104,6 +140,7 @@ def main():
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
+        pygame.quit()
         sys.exit()
     
     for e in manager.get("updateable"):
